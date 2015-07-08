6454fd8f82821a27372447312c3912fdfbd9a3ff
tomonariokubo <tomonari.okubo@gmail.com>
2015-07-08 21:57:08 +0900

    Initial Commit

---
+++ SampleSlideMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleSlideMenu
+//
+//  Created by 大久保 知成 on 2015/07/08.
+//  Copyright (c) 2015年 Griffonix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
